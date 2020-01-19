9485d5bd9738993b4b508293c568fbeafb39f5f5
JayLs <o.mishkin@squaregps.com>
2020-01-19 15:13:14 +0500

    Initial Commit

---
+++ Yozi.Lib/Yozi_Lib.h
@@ -0,0 +1,19 @@
+//
+//  Yozi_Lib.h
+//  Yozi.Lib
+//
+//  Created by Олег Мишкин on 19/01/2020.
+//  Copyright © 2020 Олег Мишкин. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Yozi_Lib.
+FOUNDATION_EXPORT double Yozi_LibVersionNumber;
+
+//! Project version string for Yozi_Lib.
+FOUNDATION_EXPORT const unsigned char Yozi_LibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yozi_Lib/PublicHeader.h>
+
+
