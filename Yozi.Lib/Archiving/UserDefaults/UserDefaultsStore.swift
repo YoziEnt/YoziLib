7fcade7ea9fb08dd5b4a8d914cb6a4f6d904e65e
JayLs <o.mishkin@squaregps.com>
2020-01-19 16:35:18 +0500

    Modify: UserDefautsStore

--- Yozi.Lib/Archiving/UserDefaults/UserDefaultsStore.swift
+++ Yozi.Lib/Archiving/UserDefaults/UserDefaultsStore.swift
@@ -7,14 +7,14 @@
 //
 
 public class UserDefaultsStore {
-    static private let defaults = UserDefaults.standard
+    private static let defaults = UserDefaults.standard
     
-    static func save <Value : Codable> (key: String, data: Value) {
+    public static func save <Value : Codable> (key: String, data: Value) {
         defaults.set(try? PropertyListEncoder().encode(data), forKey: key)
         defaults.synchronize()
     }
     
-    static func load <Value : Codable>(key: String, type: Value.Type) -> Value? {
+    public static func load <Value : Codable>(key: String, type: Value.Type) -> Value? {
         if let data = defaults.value(forKey: key) as? Data {
             if let savedData = try? PropertyListDecoder().decode(Value.self, from: data) {
                 return savedData
